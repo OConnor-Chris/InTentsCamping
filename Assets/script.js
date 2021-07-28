16968f1522aef7f3448c6574fcffec653e370d5e
Chris O'Connor <82180619+OConnor-Chris@users.noreply.github.com>
2021-07-27 22:23:38 -0500

    Console.log / GH Desktop nonsense
    APIs really are easier to look at in Postman (like Chris mentioned at the beginning of class 6.1)

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,16 @@
-let map;
+var requestUrl = "https://developer.nps.gov/api/v1/activities/parks?&api_key=xSn7ChieXuRmYI13uvMt5MVAakcIvQOihc2TvJMf"
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+
+function getApi(requestUrl) {
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+      })
 }
+
+getApi(requestUrl);
+
+// curl -X GET "https://developer.nps.gov/api/v1/activities/parks?api_key=xSn7ChieXuRmYI13uvMt5MVAakcIvQOihc2TvJMf" -H "accept: application/json"
