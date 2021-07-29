eaca023332f7d8915cfeee80e3a03845bfc923bc
ristjosh <ristjosh1@icloud.com>
2021-07-28 20:38:25 -0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,11 @@
 var searchBarEL = $('<li>').appendTo('#search')
-var requestUrl = "https://developer.nps.gov/api/v1/campgrounds/?&api_key=xSn7ChieXuRmYI13uvMt5MVAakcIvQOihc2TvJMf"
-
-function getApi(requestUrl) {
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.data[0].latLong)
-    })
-}
-
-getApi(requestUrl);
+var searchResult = document.querySelector('.search-result')
+var campsite = document.querySelector('#campsite')
+var requestUrl = 'https://developer.nps.gov/api/v1/campgrounds/?&api_key=xSn7ChieXuRmYI13uvMt5MVAakcIvQOihc2TvJMf'
 
 //When a user searches for an area
     //Search Bar
-    `
+    
     //Hit search button
     //Calling for the API
         //Calls RIBD recreation API
@@ -27,9 +17,9 @@ getApi(requestUrl);
 //They are presented with a list of campsites
     //LI
         //Dynamically create
-`
+
 //Campsites will be listed with distance from your location
-
+ 
 //Storing search results to Local Storage
-
-
+localStorage.setItem("search")
+document.getElementById("search").innerHTML = localStorage.getItem("search-result");
