13b3116ddbefb9af0b989fdde0bb678189fea996
Chris O'Connor <82180619+OConnor-Chris@users.noreply.github.com>
2021-07-28 19:39:24 -0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,18 @@
 var searchBarEL = $('<li>').appendTo('#search')
+var requestUrl = "https://developer.nps.gov/api/v1/campgrounds/?&api_key=xSn7ChieXuRmYI13uvMt5MVAakcIvQOihc2TvJMf"
 
-function getApi() {
-
+function getApi(requestUrl) {
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.data[0].latLong)
+    })
 }
+
+getApi(requestUrl);
+
 //When a user searches for an area
     //Search Bar
     `
