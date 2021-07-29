aee62a49a0318484b05bdcd2f08e00388941509b
ristjosh <ristjosh1@icloud.com>
2021-07-29 11:19:41 -0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -14,20 +14,25 @@ function getApi(requestUrl) {
 getApi(requestUrl);
 
 //When a user searches for an area
-    //Search Bar
-    `
+    //Dropdown
+    
     //Hit search button
+
     //Calling for the API
+
         //Calls RIBD recreation API
+
             //List of Campsites
+
         //Calls Google Maps API
+
             //Distance to the campsites
     //Geneorates list of campsites
     
 //They are presented with a list of campsites
     //LI
         //Dynamically create
-`
+
 //Campsites will be listed with distance from your location
 
 //Storing search results to Local Storage
