ae3b239623dcc0fada2a0add758c7fa07cc740d4
Chris O'Connor <82180619+OConnor-Chris@users.noreply.github.com>
2021-07-31 13:56:28 -0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,85 +1,97 @@
-var searchBarEL = $('<li>').appendTo('#search')
-var searchResult = document.querySelector('.search-result')
-var campsite = document.querySelector('#campsite')
-var requestUrl = 'https://developer.nps.gov/api/v1/campgrounds/?stateCode=mn&api_key=xSn7ChieXuRmYI13uvMt5MVAakcIvQOihc2TvJMf'
+var searchBarEL = $('<li>').appendTo('#search');
+var searchResult = document.querySelector('.search-result');
+var campsite = document.querySelector('#campsite');
+// var userState = $('#state-select').val();
 
-// Changes the selected state in the NPS API call
-// let userState = userInput.value
-// let requestUrl = "https://developer.nps.gov/api/v1/campgrounds/?stateCode=" + userState + "&api_key=xSn7ChieXuRmYI13uvMt5MVAakcIvQOihc2TvJMf"
-
-// requestUrl at the moment has Minnesota by default
-function getApi(requestUrl) {
+function getApi(userState) {
+    var requestUrl = "https://developer.nps.gov/api/v1/campgrounds?stateCode=" + userState + "&limit=10&api_key=kdwUFElfnyssbQAQVTTsu4o686nGIvszl3ymx0IW"
+        console.log(userState);
     fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data.data[0].addresses[0].stateCode)
-      })
+        .then(function(response){
+            return response.json();
+        })
+        .then(function (npsResponseArray) {
+           renderResults(npsResponseArray);
+        })
 }
 
+function renderResults(npsApiResponse) {
+    console.log("npsApiResponse call here");
+    console.log(npsApiResponse);
+    var respData = npsApiResponse.data;
+    for (let i = 0; i < respData.length; i++) {
+        // ? a condition that checks if the property exists
+        var resultEl = $('<li>').addClass('user-select').text(`${respData[i]?.name} ${respData[i].url}`)
+        
 
-getApi(requestUrl);
-//When a user searches for an area
-    //Search Bar
+        console.log(resultEl);
+        $('.search-results').append(resultEl);
+        
+        
+        
+    }
     
-    //Hit search button
-    //Calling for the API
-        //Calls RIBD recreation API
-            //List of Campsites
-        //Calls Google Maps API
-            //Distance to the campsites
-    //Geneorates list of campsites
+}
+
+//When user selects the campsite
+    //Generate a route to the campsite
+        //Pop-out? Second page?
     
-//They are presented with a list of campsites
-    //LI
-        //Dynamically create
+
 
 //Campsites will be listed with distance from your location
- 
-//Storing search results to Local Storage
+
+ //When a user selects a State from the dropdown list
+    //Dynamically generate a list of campsites in that state
+
+$(document).on('click', '.list', function(event){
+    var userState = $(event.target).text().trim();
+    $('#state-select').val(userState);
+    getApi(userState);
+    
+})
 
 //This is jq for the dropdown menu
 $( document ).ready(function() {
     $(".dropdown-trigger").dropdown();
 });
-// localStorage.setItem("search")
-// document.getElementById("search").innerHTML = localStorage.getItem("search-result");
+//Storing search results to Local Storage
+
+
 
 let map;
-let service;
-let infowindow;
+    let service;
+    let infowindow;
 
-function initMap() {
-  const mpls = new google.maps.LatLng(44.9778, -93.2650);
-  infowindow = new google.maps.InfoWindow();
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: mpls,
-    zoom: 15,
-  });
-  const request = {
-    query: "Museum of Contemporary Art Australia",
-    fields: ["name", "geometry"],
-  };
-  service = new google.maps.places.PlacesService(map);
-  service.findPlaceFromQuery(request, (results, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-      for (let i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-      map.setCenter(results[0].geometry.location);
+    function initMap() {
+    const mpls = new google.maps.LatLng(44.9778, -93.2650);
+    infowindow = new google.maps.InfoWindow();
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: mpls,
+        zoom: 6,
+    });
+    const request = {
+        query: 'al',
+        fields: ["name", "geometry"],
+    };
+    service = new google.maps.places.PlacesService(map);
+    service.findPlaceFromQuery(request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+        for (let i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+        map.setCenter(results[0].geometry.location);
     }
   });
 }
 
-function createMarker(place) {
-  if (!place.geometry || !place.geometry.location) return;
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
-  });
-  google.maps.event.addListener(marker, "click", () => {
-    infowindow.setContent(place.name || "");
-    infowindow.open(map);
-  });
-}
+    function createMarker(place) {
+    if (!place.geometry || !place.geometry.location) return;
+    const marker = new google.maps.Marker({
+        map,
+        position: place.geometry.location,
+    });
+    google.maps.event.addListener(marker, "click", () => {
+        infowindow.setContent(place.name || "");
+        infowindow.open(map);
+  });}
