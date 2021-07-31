3fa7614fdf046e35099d6567f2db19a6f6cad9c5
Chris O'Connor <82180619+OConnor-Chris@users.noreply.github.com>
2021-07-31 11:32:24 -0500

    Moved initMap to .js

--- Assets/script.js
+++ Assets/script.js
@@ -18,15 +18,7 @@ function getApi(requestUrl) {
       })
 }
 
-function getApi(requestUrl) {
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data.data[0].addresses[0].stateCode)
-      })
-}
+
 getApi(requestUrl);
 //When a user searches for an area
     //Search Bar
@@ -53,3 +45,41 @@ $( document ).ready(function() {
 });
 // localStorage.setItem("search")
 // document.getElementById("search").innerHTML = localStorage.getItem("search-result");
+
+let map;
+let service;
+let infowindow;
+
+function initMap() {
+  const mpls = new google.maps.LatLng(44.9778, -93.2650);
+  infowindow = new google.maps.InfoWindow();
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: mpls,
+    zoom: 15,
+  });
+  const request = {
+    query: "Museum of Contemporary Art Australia",
+    fields: ["name", "geometry"],
+  };
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      for (let i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
+
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
+  });
+}
