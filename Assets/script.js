3f2b236bdc0fe0756945298134b44c56ab36774e
Chris O'Connor <82180619+OConnor-Chris@users.noreply.github.com>
2021-08-01 16:08:54 -0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-var searchBarEL = $('<li>').appendTo('#search');
+
 var searchResult = document.querySelector('.search-result');
 var campsite = document.querySelector('#campsite');
 // var userState = $('#state-select').val();
@@ -27,15 +27,12 @@ function renderResults(npsApiResponse) {
         console.log(resultEl);
         $('.search-results').append(resultEl);
         
-        
-        
     }
-    
 }
-
 //When user selects the campsite
     //Generate a route to the campsite
         //Pop-out? Second page?
+
     
 
 
@@ -57,41 +54,50 @@ $( document ).ready(function() {
 });
 //Storing search results to Local Storage
 
+let map, infoWindow;
 
-
-let map;
-    let service;
-    let infowindow;
-
-    function initMap() {
-    const mpls = new google.maps.LatLng(44.9778, -93.2650);
-    infowindow = new google.maps.InfoWindow();
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: mpls,
-        zoom: 6,
-    });
-    const request = {
-        query: 'al',
-        fields: ["name", "geometry"],
-    };
-    service = new google.maps.places.PlacesService(map);
-    service.findPlaceFromQuery(request, (results, status) => {
-        if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-        for (let i = 0; i < results.length; i++) {
-            createMarker(results[i]);
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6,
+  });
+  infoWindow = new google.maps.InfoWindow();
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
         }
-        map.setCenter(results[0].geometry.location);
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
     }
   });
 }
 
-    function createMarker(place) {
-    if (!place.geometry || !place.geometry.location) return;
-    const marker = new google.maps.Marker({
-        map,
-        position: place.geometry.location,
-    });
-    google.maps.event.addListener(marker, "click", () => {
-        infowindow.setContent(place.name || "");
-        infowindow.open(map);
-  });}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
