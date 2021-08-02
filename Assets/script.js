a3a4ff3353095a13a175da50852afd0cf04d12ea
Chris O'Connor <82180619+OConnor-Chris@users.noreply.github.com>
2021-08-02 18:22:39 -0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -33,7 +33,43 @@ function renderResults(npsApiResponse) {
     //Generate a route to the campsite
         //Pop-out? Second page?
 
-    
+let map;
+let service;
+let infowindow;
+
+function initMap() {
+  const sydney = new google.maps.LatLng(-33.867, 151.195);
+  infowindow = new google.maps.InfoWindow();
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: sydney,
+    zoom: 8,
+  });
+  const request = {
+    query: `rome`,
+    fields: ["name", "geometry"],
+  };
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      for (let i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
+
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
+  });
+} 
 
 
 //Campsites will be listed with distance from your location
